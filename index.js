03c359a36426da8d49750954f2f62f3b173ab491
Xi Yu <xiyu@amazon.com>
2017-12-20 22:22:17 -0800

    Add api gateway

--- index.js
+++ index.js
@@ -3,5 +3,11 @@
 console.log('Loading function');
 
 exports.handler = (event, context, callback) => {
-    callback(null, 'Hello World!' + event.key1);
+    console.log('hello');
+    callback(null, {
+        statusCode: 200,
+        headers: { 'x-custom-header' : 'my custom header value' },
+        body: 'Hello World!' + event.key1
+
+    });
 };
